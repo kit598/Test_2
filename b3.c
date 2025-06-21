f6d33349ebf34e810a12b97e8f5c406ca1f64680
Kitisak Danchalorn <kdanchal@z2t7c2.42bangkok.com>
2025-06-21 22:56:13 +0700

    b3 real real 3

--- b3.c
+++ b3.c
@@ -4,6 +4,9 @@
 int main()
 {
 	printf("b3 real real");
+
+	// 21jun 22:55
+
 }
 
-// git add . && git commit -m "b3 real real 3" && git push 
+// git checkout branch03 && git add . && git commit -m "b3 real real 3" && git push 
