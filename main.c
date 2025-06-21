be42211281dcbcd6635ffe58b4e3bb17f45c3587
Kitisak Danchalorn <kdanchal@z2t7c2.42bangkok.com>
2025-06-21 23:18:18 +0700

    04branch

---
+++ main.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+	printf("21jun21:45");
+	//branch 04
+	//branch 04
+
+	//branch 04
+
+	//branch 04
+
+	//branch 04
+
+}
+
+//	git add . && git commit -m "m1" && git push 
